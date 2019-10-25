c3eb6db69e3547a052e5bd6469dcaa7df90ed00f
Jaden Marcus Ren <ruirenjerry@gmail.com>
2019-10-25 16:56:04 -0500

    Update Trie.cpp

--- daily/2019-10-24/Trie.cpp
+++ daily/2019-10-24/Trie.cpp
@@ -1,127 +1,55 @@
 #include <iostream>
-#include <stdlib.h>
-#include<map>
-
-# define CHAR_SIZE 128
-
-
+#include <string>
+#define ALPHABET_SIZE 26
 using namespace std;
 
-class Trie
+typedef struct TrieNode
 {
-public:
-    bool isLeaf;
-    Trie* Character[CHAR_SIZE];
-    
-    // constructor
-    Trie()
+    int count;           // count number
+    TrieNode * children[ALPHABET_SIZE];   // children
+} * trie;
+
+TrieNode* create_trie_node()
+{
+    TrieNode* pNode = new TrieNode;
+    pNode->count = 0;
+    for(int i = 0; i < ALPHABET_SIZE; i++)
     {
-        this->isLeaf = false;
-
-        for(int i = 0; i < CHAR_SIZE; i++)
-            this->Character[i] = nullptr;
+        pNode->children[i] = NULL;
     }
-
-    void insert(std::string);
-    bool deletion(Trie*&, std::string);
-    bool search(std::string);
-    bool haveChildren(Trie const*);
+    return pNode;
 };
 
-void Trie::insert(string key)
+void trie_insert(trie root, char* key)
 {
-    // start from root node
-    Trie* curr = this;
-    for (int i = 0; i < key.length(); i++)
+    TrieNode* node = root;
+    char* p = key;
+    while(*p)
     {
-        // create a new node if path doesn't exists
-        if (curr->Character[key[i]] == nullptr)
-            curr->Character[key[i]] = new Trie();
-        
-        // go to the next node
-        curr = curr->Character[key[i]];
-    }
-    curr->isLeaf = true;
-}
-
-bool Trie::search(string key)
-{
-    // return false if Trie is empty
-    if (this == nullptr) return false;
-
-    Trie* curr = this;
-    for (int i = 0; i < key.length(); i++)
-    {
-        curr = curr->Character[key[i]];
-
-        if (curr == nullptr) return false;
-    }
-    return curr->isLeaf;
-}
-
-bool Trie::haveChildren(Trie const* curr)
-{
-    for (int i = 0; i < CHAR_SIZE; i++)
-    {
-        if (curr->Character[i]) return true;
-    }
-}
-
-bool Trie::deletion(Trie*& curr, std::string key)
-{
-    if (curr == nullptr) return false;
-
-    // if we have not reached the end of the key
-
-    if (key.length())
-    {
-        if (curr != nullptr &&
-        curr->Character[key[0]] != nullptr &&
-        deletion(curr->Character[key[0]], key.substr(1)) &&
-        curr->isLeaf == false)
+        if (node->children[*p-'a']==NULL)
         {
-            if (!haveChildren(curr))
-            {
-                delete curr;
-                curr = nullptr;
-                return true;
-            }
-            else{
-                return false;
-            }
+            node->children[*p-'a'] = create_trie_node();
         }
+        node = node->children[*p-'a'];
+        ++p;
     }
-
-
-    // if we have reached the end of the key
-    if (key.length() == 0 && curr->isLeaf)
-    {
-        // if current node is a leaf node and do not have any children
-        if (!haveChildren(curr))
-        {
-            delete curr;
-            curr = nullptr;
-            return true;
-        }
-        else
-        {
-            curr->isLeaf = false;
-            return false;
-        } 
-    }
-
-    return false;
+    node->count += 1;
 }
 
-int main()
+int trie_search(trie root, char* key)
 {
-    Trie* head = new Trie();
-
-    head->insert("hello");
-
-    std::cout << head->search("hello") << " ";
-
-
-}
-
-
+    TrieNode* node = root;
+    char* p = key;
+    while(*p && node!= NULL)
+    {
+        node = node -> children[*p-'a'];
+        ++p;
+    }
+    if (node == NULL)
+        return 0;
+    else
+    {
+        return node -> count;
+    }
+    
+};
