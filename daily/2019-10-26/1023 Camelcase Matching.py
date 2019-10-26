415c74d8212150ef79718e93fc72ab58ef0c57fc
Jaden Marcus Ren <ruirenjerry@gmail.com>
2019-10-26 13:12:05 -0500

    Update 1023 Camelcase Matching.py

--- daily/2019-10-26/1023 Camelcase Matching.py
+++ daily/2019-10-26/1023 Camelcase Matching.py
@@ -3,7 +3,7 @@
 # Two pointer for the solution
 # Hash map is not efficient to control the pointer
 class solution:
-    
+
     def ifcamelMatch(self, query, pattern):
         # define two pointers
         p_idx, p_len = 0, len(pattern)
@@ -20,4 +20,25 @@ class solution:
     def camelMatch(self, queries, pattern):
         return [self.ifcamelMatch(query, pattern) for query in queries]
             
+
+# solution 2, we can use regular expression for pattern split
+
+    def camelMatch2(self, queries, pattern):
+        import re
+        ps = re.findall("[A-Z][a-z]*", pattern)
+        #num = len(queries)
+        res = []
+
+        for q in queries:
+            qs = re.findall("[A-Z][a-z]*", q)
+            hasFind = False
+            if len(ps) == len(qs):
+                if all(self.isSubSeq(q,p) for (q, p) in zip(qs, ps)):
+                    hasFind = True
+            res.append(hasFind)
+        return res
+
+    def isSubSeq(self, qu, pa):
+        qu = iter(qu)
+        return all(p in qu for p in pa)
 
