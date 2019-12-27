98f06ef6f53d5da2694b635833ff88c868d5beaf
Jaden Marcus Ren <ruirenjerry@gmail.com>
2019-12-27 13:03:47 -0800

    27 leetcode

---
+++ daily/2019-12-27/344 Reverse String.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def reverseString(self, s):
+        s.reverse()
+    
+    def reverseString(self, s):
+        # two pointer solutions
+        left, right = 0, len(s)-1
+        while left <= right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        
