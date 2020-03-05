0fa35bf020fe31392ef90cbb44a9abccfd346559
Jaden Marcus Ren <ruirenjerry@gmail.com>
2020-03-04 23:54:58 -0600

    tree

---
+++ daily/2020-03-04/112 Path Sum.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def hasPathSum(self, root, sum):
+        res = []
+        if not root:
+            return True
+
+        return self.helper(root, sum, res) or self.hasPathSum(root.left, sum, res) or self.hasPathSum(root.right, sum, res)
+        
+
+    def helper(self, root, sum, res):
+        
+        if not root:
+            return sum(root) == sum
+
+        return self.helper(root.left, sum, res.append(root.val)) or self.helper(root.right, sum, res.append(root.val))
+
+
