56e4dd02d9c1ee6a723276ff10ab84be82f61597
Jaden Marcus Ren <ruirenjerry@gmail.com>
2020-03-16 23:19:12 -0500

    sql

--- SQL/2020-03-16/570 Managers with at Least 5 Direct Reports.sql
+++ SQL/2020-03-16/570 Managers with at Least 5 Direct Reports.sql
@@ -1,9 +1,22 @@
-SELECT E1.name
-FROM
-    Employee E1
-INNER JOIN
-    Employee E2
-ON E1.Id = E2.ManagerId
-GROUP BY E2.ManagerId
-HAVING COUNT(E2.ManagerId) >= 5
+-- SELECT E1.name
+-- FROM
+--     Employee E1
+-- INNER JOIN
+--     Employee E2
+-- ON E1.Id = E2.ManagerId
+-- GROUP BY E2.ManagerId
+-- HAVING COUNT(E2.ManagerId) >= 5
 
+SELECT 
+    name
+FROM
+    Employee
+WHERE
+    Id in 
+    (
+        SELECT ManagerId
+        FROM   
+            Employee
+        GROUP BY ManagerId
+        HAVING COUNT(*) >= 5
+    )
