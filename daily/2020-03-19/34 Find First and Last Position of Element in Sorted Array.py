a6f07940f0958bcef25420bf03dd99f83449360a
Jaden Marcus Ren <ruirenjerry@gmail.com>
2020-03-19 22:49:23 -0500

    binary search

--- daily/2020-03-19/34 Find First and Last Position of Element in Sorted Array.py
+++ daily/2020-03-19/34 Find First and Last Position of Element in Sorted Array.py
@@ -19,7 +19,6 @@ class Solution:
             elif nums[right] != target:
                 right -= 1
 
-            
         if len(res) == 2:
             return res
         elif len(res) == 0:
@@ -28,4 +27,59 @@ class Solution:
             return [single] * 2
         
     
+    # time complexity o(n)
+    # space complexity o(1)
+
+    def searchRange2(self, nums, target):
+
+        # binary search algorithm
+        res = None
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                left, right = mid, mid
+                while left >= 0 and nums[left] == target:
+                    left -= 1
+                while right < len(nums) and nums[right] == target:
+                    right += 1
+                res = [left + 1, right - 1]
+                break
+            elif nums[mid] > target:
+                right = mid - 1   
+            elif nums[mid] < target:
+                left = mid + 1
         
+        if not res:
+            return [-1, -1]
+        return res
+    
+    # time complexity o(n + logn)
+    # space complexity o(n)
+
+    def searchRange3(self, nums, target):
+
+        left_idx = self.extreme_insertation_index(nums, target, True)
+        if left_idx == len(nums) or nums[left_idx] != target:
+            return [-1, -1]
+        
+        return [left_idx, self.extreme_insertation_index(nums, target, False) -1]
+
+    def extreme_insertation_index(self, nums, target, boolean):
+        left, right = 0, 1
+
+        while left <= right:
+            mid = (left+ right) //  2
+            # find the left most value for the algorithm
+            # find the right most value for the algorithm
+            if nums[mid] > target or (boolean and target == nums[mid]):
+                right = mid -1
+            else:
+                left = mid + 1
+
+        return left
+
+    # time complexity o(logn)
+    # space complexity o(1)
+
+    
