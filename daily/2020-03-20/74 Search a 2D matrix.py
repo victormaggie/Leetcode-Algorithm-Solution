95fb420255e25f96584801525010d9cf458d7b73
Jaden Marcus Ren <ruirenjerry@gmail.com>
2020-03-21 11:31:26 -0500

    2D binary

--- daily/2020-03-20/74 Search a 2D matrix.py
+++ daily/2020-03-20/74 Search a 2D matrix.py
@@ -5,3 +5,72 @@ def flatten(L):
         except TypeError:
             yield item
     
+# python version 3.3 or beyond
+
+def flatten(L):
+    for object in L:
+        if hasattr(object, '__iter__') and not isinstance(object, str):
+            yield from flatten(object)
+        else:
+            yield(object)
+
+# cannot iterate flatten [1, 2, [2, 2, [3, 3]]]
+
+
+def flatten(L):
+    import functools
+    X = functools.reduce(lambda x, y: x + y, L)
+    return X
+
+# cannot solve this problem [1, 2, [2, 2, [3, 3]]]
+
+
+
+class Solution:
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        # edge case
+        if not matrix or not matrix[0]:
+            return False
+        if matrix[-1][-1] < target:
+            return False
+        if matrix[-1][-1] == target:
+            return True
+        row, column = len(matrix)-1, len(matrix[0])-1    
+        up, down = 0, row
+        left, right = 0, column
+        
+        search_row = -float('inf')
+        while up <= down:
+            mid = (up + down) // 2
+            if matrix[mid][column] == target:
+                return True
+            
+            elif matrix[mid][column] > target:
+                up = mid + 1
+            
+            elif matrix[mid][column] < target:
+                down = mid - 1
+            
+            if matrix[mid][column] > target and (matrix[mid-1][column] < target or mid - 1 <0 ):
+                search_row = mid
+                break
+        
+        return self.binary_search(matrix, search_row, target)
+            
+    def binary_search(self, matrix, row, target):
+        left , right = 0, len(matrix[0])-1
+        
+        while left <= right:
+            mid = (left + right)//2
+            if matrix[row][mid] == target:
+                return True
+            
+            elif matrix[row][mid] > target:
+                right = mid - 1
+            
+            elif matrix[row][mid] < target:
+                left = mid + 1
+        
+        return False
