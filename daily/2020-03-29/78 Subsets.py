8d88f67f5d41dd042972e9c4d0d50fdc29de3a48
Jaden Marcus Ren <ruirenjerry@gmail.com>
2020-03-29 22:57:21 -0500

    backtracking

---
+++ daily/2020-03-29/78 Subsets.py
@@ -0,0 +1,20 @@
+
+class Solution:
+
+    # no stop condition
+    # use a global to contain all the path
+    def subsets(self, nums):
+        self.res = []
+        temp = []
+        self.dfs(nums, 0, temp)
+        return self.res
+    
+    def dfs(self, nums, idx, temp):
+        self.res.append(temp[:])
+        for i in range(idx, len(nums)):
+            temp.append(nums[i])
+            self.dfs(nums, i+1, temp)
+            temp.pop()
+
+# time complexity o(n * n^2)
+# space complexity o(2^n)
