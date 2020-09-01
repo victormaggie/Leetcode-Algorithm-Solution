bf09f094f2733060d07248d1faad36f56f35b066
Jaden Marcus Ren <ruirenjerry@gmail.com>
2020-09-01 11:01:33 -0400

    js

--- JavaScript/24. Swap Nodes in Pairs.js
+++ JavaScript/24. Swap Nodes in Pairs.js
@@ -20,3 +20,47 @@ var swapPairs = function(head) {
 }
 
 // time complexity o(n)
+
+
+// We can only use one node here
+// this code is neat !!
+
+var swapPairs = function(head) {
+    let dummy = new ListNode(0);
+    dummy.next = head;
+    curr = dummy;
+
+    while (curr.next && curr.next.next) {
+        let first_node = curr.next;
+        let second_node = curr.next.next;
+
+        // swap here
+        curr.next = second_node;
+        second_node.next = first_node;
+        first_node.next = second_node.next;
+
+        // move curr node
+        curr = first_node;
+    }
+    return dummy.next;
+}
+
+
+// we can also use the recursion method
+
+var swapPairs = function(head) {
+    if ((!head) || (!head.next)) return head;
+
+    // first node and second node
+    let first_node = head;
+    let second_node = head.next;
+
+    // swapping here
+    first_node.next = swapPairs(second_node.next);
+    second_node.next = first_node;
+
+    return second_node;
+}
+
+// time complexity o(n)
+// space complexity o(n)
