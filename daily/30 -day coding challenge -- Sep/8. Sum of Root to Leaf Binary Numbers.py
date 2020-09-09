9f5c51419cea0e033bd2cd417cbb45567a2ad117
Jaden Marcus Ren <ruirenjerry@gmail.com>
2020-09-08 23:24:35 -0700

    tree

---
+++ daily/30 -day coding challenge -- Sep/8. Sum of Root to Leaf Binary Numbers.py
@@ -0,0 +1,29 @@
+
+class Solution:
+
+    def sumRootToLeaf(self, root: TreeNode) -> int:
+
+        if not root: return 0
+
+        self.sum = 0
+
+        def dfs(root, path):
+
+            if not root.left and not root.right:
+                self.sum += int(path, 2)
+                return 
+
+            if root.left:
+                dfs(root.left, path + str(root.left.val))
+
+            if root.right:
+                dfs(root.right, path + str(root.right.val))
+        
+        dfs(root, str(root.val))
+
+        return self.sum
+
+
+# time complexity o(n)
+# space complexity o(n)
+
